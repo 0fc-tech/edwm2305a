4c263e871b7198907a21cd6b1963ddd72a006b94
riderofzion <quentinrouet@hotmail.fr>
2023-10-09 11:31:07 +0200

    Démonstration Permissions dangereuses

--- mod6intent/src/main/java/com/eniecole/mod6intent/MainActivity.kt
+++ mod6intent/src/main/java/com/eniecole/mod6intent/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.eniecole.mod6intent
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -8,12 +9,26 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ isGranted ->
+        if( isGranted){
+            val intentCallPhone = Intent(Intent.ACTION_CALL, Uri.parse("tel:012345678"))
+            startActivity(intentCallPhone)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissionLauncher.launch(Manifest.permission.CALL_PHONE)
+
         //INTENT EXPLICITE => Envoyer l'utilisateur sur une activité dont le nom est connue
         //val intentToSecond = Intent(this, SecondActivity::class.java)
         //startActivity(intentToSecond)
@@ -27,9 +42,9 @@ class MainActivity : AppCompatActivity() {
         //val intentToENI = Intent(Intent.ACTION_VIEW, Uri.parse("geo:47.225934,-1.6176846"))
         ///startActivity(intentToENI)
         //registerReceiver(ReceiverLowBattery(), IntentFilter(Intent.ACTION_BATTERY_LOW))
-        val intentToEMail = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:contact@jeanmichel.com"))
-        startActivity(intentToEMail)
-        registerReceiver(ReceiverLowBattery(), IntentFilter(Intent.ACTION_BATTERY_LOW))
+        //val intentToEMail = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:contact@jeanmichel.com"))
+        //startActivity(intentToEMail)
+        //registerReceiver(ReceiverLowBattery(), IntentFilter(Intent.ACTION_BATTERY_LOW))
     }
 }
 class ReceiverLowBattery : BroadcastReceiver(){
